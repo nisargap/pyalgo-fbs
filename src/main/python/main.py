37e3e4c1144c047bc3404f56ada42fd9267ef019
Nisarga Patel <np8@umbc.edu>
2018-09-27 17:47:37 -0400

    Completed basic UI which includes two calendars, two text fields and a button

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,18 +1,19 @@
 from fbs_runtime.application_context import ApplicationContext,  cached_property
 from PyQt5.QtCore import Qt, QDate
-from PyQt5.QtWidgets import QWidget, QMainWindow, QLabel, QGridLayout, QVBoxLayout, QCalendarWidget
+from PyQt5.QtWidgets import QWidget, QLineEdit, QMainWindow, QLabel, QGridLayout, QVBoxLayout, QCalendarWidget, QPushButton
 
 import sys
 
 class AppContext(ApplicationContext):           # 1. Subclass ApplicationContext
     def run(self):
+        self.window.show()
         stylesheet = self.get_resource('styles.qss')
         self.app.setStyleSheet(open(stylesheet).read())
-        self.window.show()
         return self.app.exec_()
     @cached_property
     def window(self):
-        return MainWindow()
+        mw = MainWindow()
+        return mw
 
 def createCalendar(ref, clickAction):
     cal = QCalendarWidget(ref)
@@ -26,6 +27,7 @@ class MainWindow(QWidget):
 
         layout = QGridLayout()
 
+        
         calFrom = createCalendar(self, self.showDate)
         calTo = createCalendar(self, self.showDateTwo)
 
@@ -56,10 +58,44 @@ class MainWindow(QWidget):
 
         layout.addWidget(self.lbl, 2, 0)
         layout.addWidget(self.lblTwo, 2, 1)
+
+        lineEditLayout = QVBoxLayout()
+        lineEditLayout.setContentsMargins(0,0,0,0)
+        lineEditLayout.setSpacing(5)
+
+        tickerLbl = QLabel(self)
+        tickerLbl.setMaximumHeight(20)
+        tickerLineEdit = QLineEdit(self)
+        tickerLineEdit.setMaximumHeight(20)
+        tickerLineEdit.setAlignment(Qt.AlignTop)
+        tickerLbl.setText("Enter ticker symbol: ")
+        tickerLbl.setAlignment(Qt.AlignTop)
+        datasetLbl = QLabel(self)
+        datasetLbl.setMaximumHeight(20)
+        datasetLbl.setText("Enter the Quandl Dataset Code: ")
+        datasetLbl.setAlignment(Qt.AlignTop)
+
+        getDataBtn = QPushButton(self)
+        getDataBtn.setText("Download CSV Data")
+
+        quandlLineEdit = QLineEdit(self)
+        quandlLineEdit.setMaximumHeight(20)
+        quandlLineEdit.setAlignment(Qt.AlignTop)
+
+        lineEditLayout.addWidget(tickerLbl, 0, Qt.AlignTop)
+        lineEditLayout.addWidget(tickerLineEdit, 0, Qt.AlignTop)
+        lineEditLayout.addWidget(datasetLbl)
+        lineEditLayout.addWidget(quandlLineEdit)
+        lineEditLayout.addWidget(getDataBtn)
+
+        layout.addLayout(lineEditLayout, 3, 0)
+
+
+        layout.setContentsMargins(20,20,20,20)
+        layout.setSpacing(10)
         self.setLayout(layout)
         self.setGeometry(300, 300, 350, 300)
         self.setWindowTitle('Equities data')
-        self.show()
 
     def showDate(self, date): 
         self.lbl.setText("Selected from date: " + date.toString())
